524f014cc2fa063fa266491d4866b4c3e3e9b50d
JonathanAtancuri3218 <JOTA5613jhon>
2020-12-12 02:25:56 -0500

    cambiosA

--- banco/src/main/java/ec/ups/edu/banco/modelo/Usuario.java
+++ banco/src/main/java/ec/ups/edu/banco/modelo/Usuario.java
@@ -1,5 +1,7 @@
 package ec.ups.edu.banco.modelo;
 
+import java.util.List;
+@Entity
 public class Usuario {
 	
 	
@@ -12,62 +14,13 @@ public class Usuario {
 	private String usuarioNombre;
 	private String contraseña;
 	private String tipoUsuario;
+	@OneToMany(cascade = {CascadeType.ALL}, fetch = FetchType.EAGER, mappedBy = "detalleS")
+	private List<DetalleSesion> listaDS;
+	@OneToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "numCuenta")
+	private Cuenta cuenta;
 	
 	
-	public String getCedula() {
-		return cedula;
-	}
-	public void setCedula(String cedula) {
-		this.cedula = cedula;
-	}
-	public String getNombre() {
-		return nombre;
-	}
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-	public String getApellido() {
-		return apellido;
-	}
-	public void setApellido(String apellido) {
-		this.apellido = apellido;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getTelefono() {
-		return telefono;
-	}
-	public void setTelefono(String telefono) {
-		this.telefono = telefono;
-	}
-	public String getDireccion() {
-		return direccion;
-	}
-	public void setDireccion(String direccion) {
-		this.direccion = direccion;
-	}
-	public String getUsuarioNombre() {
-		return usuarioNombre;
-	}
-	public void setUsuarioNombre(String usuarioNombre) {
-		this.usuarioNombre = usuarioNombre;
-	}
-	public String getContraseña() {
-		return contraseña;
-	}
-	public void setContraseña(String contraseña) {
-		this.contraseña = contraseña;
-	}
-	public String getTipoUsuario() {
-		return tipoUsuario;
-	}
-	public void setTipoUsuario(String tipoUsuario) {
-		this.tipoUsuario = tipoUsuario;
-	}
-
-
+	
+	
 }
