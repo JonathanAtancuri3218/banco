73fc4221184e1429a511e10be92934fc767747d9
JonathanAtancuri3218 <JOTA5613jhon>
2020-12-12 01:35:39 -0500

    cambios

--- banco/src/main/java/ec/ups/edu/banco/modelo/Usuario.java
+++ banco/src/main/java/ec/ups/edu/banco/modelo/Usuario.java
@@ -1,5 +1,72 @@
 package ec.ups.edu.banco.modelo;
 
 public class Usuario {
+	
+	private String cedula;
+	private String nombre;
+	private String apellido;
+	private String email;
+	private String telefono;
+	private String direccion;
+	private String usuarioNombre;
+	private String contraseña;
+	private String tipoUsuario;
+	
+	
+	public String getCedula() {
+		return cedula;
+	}
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	public String getUsuarioNombre() {
+		return usuarioNombre;
+	}
+	public void setUsuarioNombre(String usuarioNombre) {
+		this.usuarioNombre = usuarioNombre;
+	}
+	public String getContraseña() {
+		return contraseña;
+	}
+	public void setContraseña(String contraseña) {
+		this.contraseña = contraseña;
+	}
+	public String getTipoUsuario() {
+		return tipoUsuario;
+	}
+	public void setTipoUsuario(String tipoUsuario) {
+		this.tipoUsuario = tipoUsuario;
+	}
+
 
 }
